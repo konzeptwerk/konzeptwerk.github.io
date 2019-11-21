a0f23c49ea3a0d1c265bf01053a4311b54d9eb44
Nick Petzoldt <nick.petzoldt@aboutsource.net>
2019-11-21 16:52:59 +0100

    initial static page

---
+++ wp-content/themes/bigfeature/library/js/image-maxwidth.js
@@ -0,0 +1,2 @@
+// Sets the max width of the images in the BigFeature theme http://vfxdude.com
+jQuery(function($){	$("#container img").not(".postimage img").not("img.avatar").not(".ngg-gallery-thumbnail img").not(".button img").not(".showcase img").addClass('maxwidth'); });
